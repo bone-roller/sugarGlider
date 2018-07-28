e0a6f40ed7aac6e3d7e5e0f0916a26c216d600f7
John Goossen <41789183+bone-roller@users.noreply.github.com>
2018-07-28 17:44:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,6 @@ function createAddWindow() {
 }
 // catch item:add
 ipcMain.on('item:add', function(e, item) {
-    console.log(item);
     mainWindow.webContents.send('item:add', item);
     addWindow.close();
     
